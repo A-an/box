5b77c32f8b84cc1b9e702d56f2ae7d85f7fbe50e
A-an <36506662+A-an@users.noreply.github.com>
2018-12-18 20:08:40 +0000

    Create google.js

---
+++ google.js
@@ -0,0 +1,12 @@
+<script>
+  (function() {
+    var cx = '013036898865932059220:nbek2k9xdiw';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
