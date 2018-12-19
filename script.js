8bcfb3c6463a80bc1c9181b4f1056379ce359d00
A-an <36506662+A-an@users.noreply.github.com>
2018-12-19 12:17:33 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+  (function() {
+    var cx = &#39;013036898865932059220:nbek2k9xdiw&#39;;
+    var gcse = document.createElement(&#39;script&#39;);
+    gcse.type = &#39;text/javascript&#39;;
+    gcse.async = true;
+    gcse.src = &#39;https://cse.google.com/cse.js?cx=&#39; + cx;
+    var s = document.getElementsByTagName(&#39;script&#39;)[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+<gcse:search/>
